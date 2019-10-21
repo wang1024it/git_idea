d4eaf9f71ca203736db5f0e1bcb77f6dfe86db4d
wang1024it <52960802+wang1024it@users.noreply.github.com>
2019-10-21 19:53:16 +0800

    git send local repository

---
+++ sss/src/com/wyy/git/demo.java
@@ -0,0 +1,12 @@
+package com.wyy.git;
+
+/**
+ * @author 秃头集团王某
+ * @company 秃头公司
+ * @create 2019-10-21  19:49
+ */
+public class demo {
+    public static void main(String[] args) {
+        System.out.println("git idea test");
+    }
+}
